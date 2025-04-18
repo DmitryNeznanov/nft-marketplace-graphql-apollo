c47d197dbac64da1843ec91926e0bdde0b4fd04d
Morojenk0 <110555676+Morojenk0@users.noreply.github.com>
2025-04-18 20:50:51 +0500

    Offer

--- app/components/home/Offer.tsx
+++ app/components/home/Offer.tsx
@@ -1,17 +1,31 @@
+import Link from "next/link"
+
 export default function Offer() {
   return (
-    <section className="relative from-white/0 to-accent">
-      <div className="absolute w-full h-full bg-[url('/offer.png')] -z-10"></div>
-      <div className="contianer">
+    <section className="relative pb-[40px] pt-[120px] md:pt-[360px] md:pb-[60px] bg-gradient-to-b from-white/0 to-accent">
+      <div className="absolute left-0 top-0 w-full h-full bg-[url('/offer-mobile.png')] md:bg-[url('/offer-laptop.png')] lg:bg-[url('/offer-desktop.png')] bg-cover bg-center -z-[9999]"></div>
+      <div className="container">
         <div className="max-w-sm md:max-w-full mx-auto">
-          <article>
-            <div className="w-max py-[10px] px-[20px] bg-black rounded-full">
-              <p className="p-sans">Shroomie</p>
+          <article className="flex flex-col gap-[30px] md:flex-row justify-between">
+            <div className="contents md:flex flex-col gap-[30px]">
+              <div className="w-max py-[10px] px-[20px] bg-black rounded-full order-1">
+                <p className="flex items-center p-sans before:content-[url('/heroAvatar.png')] before:w-[24px] before:h-[24px] before:mr-[12px]">
+                  Shroomie
+                </p>
+              </div>
+              <h2 className="font-work-sans font-semibold text-[38px]/[120%] lg:text-[51px]/[110%] order-2">
+                Magic Mashrooms
+              </h2>
+              <div className="order-4 md:order-3">
+                <Link
+                  className="w-full md:w-max button-white"
+                  href="#"
+                >
+                  see NFT
+                </Link>
+              </div>
             </div>
-            <h2 className="flex items-center font-work-sans font-semibold text-[38px]/[120%] lg:text-[51px]/[110%] before:content-[url('/heroAvatar.png')] before:w-[24px] before:h-[24px] before:mr-[12px]">
-              Magic Mashrooms
-            </h2>
-            <div className="w-max p-[30px] rounded-[20px] bg-black-white/50 overflow-hidden">
+            <div className="w-full h-max md:w-max p-[30px] flex flex-col items-center md:items-start self-end rounded-[20px] bg-black-white/50 overflow-hidden order-3 md:order-3">
               <p className="font-space-mono font-normal text-[12px]/[120%]">
                 Auction ends in:
               </p>
