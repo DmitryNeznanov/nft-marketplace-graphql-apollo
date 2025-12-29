e3bb564f733c0d657913e26cb031e07e71443ea2
DmitryNeznanov <fapah123@gmail.com>
2025-12-29 20:04:18 +0500

    fix small build bug

--- app/api/graphql/route.ts
+++ app/api/graphql/route.ts
@@ -1,38 +1,38 @@
 // app/api/graphql/route.ts
-import { ApolloServer } from "@apollo/server"
-import { startServerAndCreateNextHandler } from "@as-integrations/next"
-import type { NextRequest } from "next/server"
-import jwt from "jsonwebtoken"
-import typeDefs from "@/graphql/typeDefs"
-import resolvers from "@/graphql/resolvers"
-import { setCookiePlugin } from "@/lib/cookies/setCookiePlugin"
-import cookie from "cookie"
+import { ApolloServer } from "@apollo/server";
+import { startServerAndCreateNextHandler } from "@as-integrations/next";
+import type { NextRequest } from "next/server";
+import jwt from "jsonwebtoken";
+import typeDefs from "@/graphql/typeDefs";
+import resolvers from "@/graphql/resolvers";
+import { setCookiePlugin } from "@/lib/cookies/setCookiePlugin";
+import cookie from "cookie";
 
-const JWT_SECRET = process.env.JWT_SECRET || "your_secret"
+const JWT_SECRET = process.env.JWT_SECRET || "your_secret";
 
 const server = new ApolloServer<ServerContext>({
   typeDefs,
   resolvers,
   plugins: [setCookiePlugin()],
-})
+});
 
-export function getAccountFromRequest(req: Request): ServerContext["account"] {
-  const token = cookie.parse(req.headers.get("cookie") || "").token
+function getAccountFromRequest(req: Request): ServerContext["account"] {
+  const token = cookie.parse(req.headers.get("cookie") || "").token;
   if (!token) {
-    console.log("No token found in request or token is empty")
-    return null
+    console.log("No token found in request or token is empty");
+    return null;
   }
 
   try {
-    const decoded = jwt.verify(token, JWT_SECRET) as { accountId: string }
-    return decoded.accountId ? { accountId: decoded.accountId, token } : null
+    const decoded = jwt.verify(token, JWT_SECRET) as { accountId: string };
+    return decoded.accountId ? { accountId: decoded.accountId, token } : null;
   } catch {
     if (process.env.NODE_ENV === "development") {
-      console.log(`Invalid token: ${token}`)
+      console.log(`Invalid token: ${token}`);
     } else {
-      console.log("Invalid or expired token")
+      console.log("Invalid or expired token");
     }
-    return null
+    return null;
   }
 }
 
@@ -41,7 +41,7 @@ const handler = startServerAndCreateNextHandler(server, {
     account: getAccountFromRequest(req),
     setCookies: [],
   }),
-})
+});
 export async function POST(req: NextRequest) {
-  return handler(req)
+  return handler(req);
 }
